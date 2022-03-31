58f144bb6ad2206f7601de4e75da49c94f4bdec0
francisamidu <francisamidu124@gmail.com>
2022-04-01 00:13:33 +0200

    Update config.ts

--- config.ts
+++ config.ts
@@ -2,7 +2,7 @@ import ERC1155ContractAbi from "./artifacts/contracts/ERC1155NFT.sol/ERC1155NFT.
 import ERC721ContractAbi from "./artifacts/contracts/ERC721NFT.sol/ERC721NFT.json";
 
 const ERC1155NFTAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
-const ERC721NFTAddress = "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512";
+const ERC721NFTAddress = "0x30e3fB6DC335b68eC9D16EC930597db3136c1112";
 
 const ERC1155Abi = ERC1155ContractAbi.abi;
 const ERC721Abi = ERC721ContractAbi.abi;
